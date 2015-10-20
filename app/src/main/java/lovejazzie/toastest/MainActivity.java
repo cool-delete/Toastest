4bd6b433a9f27901840c84ecb1747e1c45469d82
cool-delete <kk520149492@gmail.com>
2015-10-21 02:31:39 +0800

    first commit

---
+++ app/src/main/java/lovejazzie/toastest/MainActivity.java
@@ -0,0 +1,26 @@
+package lovejazzie.toastest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button my;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        my = (Button) findViewById(R.id.show_toast);
+        my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "显示一个短", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
